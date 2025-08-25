e10b62ea464069912899caa49dc7e7e3b451127a
jaksonnascimento <jaksonjnjc@gmail.com>
2025-08-24 22:12:58 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE = "economigo-cache-v1";
+const ASSETS = [
+  "./calculadora_renda_fixa_economigo_v4.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k !== CACHE ? caches.delete(k) : null)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
